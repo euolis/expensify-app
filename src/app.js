1149c3af3c36112374883cffff1951ae1b66bd41
Michael James <mjames@atp.com>
2018-07-10 16:59:40 -0500

    fixed misspellings

--- src/app.js
+++ src/app.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }));
+store.dispatch(addExpense({ description: 'Power', amount: 2020, createdAt: -2000 }));
+store.dispatch(addExpense({ description: 'Water', amount: 200, createdAt: -25000 }));
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
