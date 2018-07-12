bf1ecd4aa166b6395549b753e901e717fdbb1185
Michael James <mjames@atp.com>
2018-07-12 16:02:28 -0500

    added test suite for ExpenseDashboardPage component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 
-const ExspenseDashboardPage = () => (
+const ExpenseDashboardPage = () => (
     <div>
         <ExpenseListFilters />
         <ExpenseList />
     </div>
 );
 
-export default ExspenseDashboardPage;
+export default ExpenseDashboardPage;
