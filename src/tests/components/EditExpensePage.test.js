9c5f3b4c5ef4b4af449352fee549ccb7a771d0f1
Michael James <mjames@atp.com>
2018-07-12 17:48:39 -0500

    added test suite for EditExpensePage component

---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { EditExpensePage } from '../../components/EditExpensePage';
+import expenses from '../fixtures/expenses';
+
+let editExpense, removeExpense, history, wrapper;
+
+beforeEach(() => {
+    editExpense = jest.fn();
+    removeExpense = jest.fn();
+    history = { push: jest.fn() };
+    wrapper = shallow(<EditExpensePage editExpense={editExpense} removeExpense={removeExpense} history={history} />);
+});
+
+test('should render EditExpensePage correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+});
