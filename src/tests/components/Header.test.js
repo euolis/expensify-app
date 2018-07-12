222f078deddc4d1dd93120dc13d547ce5a156af5
Michael James <mjames@atp.com>
2018-07-12 16:03:54 -0500

    updated Header test suite

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,5 +4,5 @@ import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').text()).toBe('Expensify');
+    expect(wrapper).toMatchSnapshot();
 });
