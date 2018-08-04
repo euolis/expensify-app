d6e2455bd202531a90414e71d218fbdf3af1e756
Michael James <m.james.cs@outlook.com>
2018-08-04 11:28:48 -0500

    added test suite for EditExpensePage component

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -9,9 +9,30 @@ beforeEach(() => {
     editExpense = jest.fn();
     removeExpense = jest.fn();
     history = { push: jest.fn() };
-    wrapper = shallow(<EditExpensePage editExpense={editExpense} removeExpense={removeExpense} history={history} />);
+    wrapper = shallow(
+        <EditExpensePage
+            editExpense={editExpense}
+            removeExpense={removeExpense}
+            history={history}
+            expense={expenses[2]}
+        />
+    );
 });
 
 test('should render EditExpensePage correctly', () => {
     expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle removeExpense', () => {
+    wrapper.find('button').simulate('click');
+    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(removeExpense).toHaveBeenLastCalledWith({
+        id: expenses[2].id
+    });
+});
+
+test('should handle editExpense', () => {
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
