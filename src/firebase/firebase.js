bbce49e833888656bec0492294d9c21d13869ed4
Michael James <m.james.cs@outlook.com>
2018-09-02 11:39:16 -0500

    removed plain text env settings for firebase due to security concerns

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBOO_ZPr5Sc2mCgzHo9jnFtZp36e7KDioM",
-    authDomain: "expensify-5ec78.firebaseapp.com",
-    databaseURL: "https://expensify-5ec78.firebaseio.com",
-    projectId: "expensify-5ec78",
-    storageBucket: "expensify-5ec78.appspot.com",
-    messagingSenderId: "974128289327"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
