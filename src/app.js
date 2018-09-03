bc6f7c60c57384f94e4d6d5e3d850aa60ef55684
Michael James <m.james.cs@outlook.com>
2018-09-03 11:05:19 -0500

    setup actions to set intial expenses from firebase to redux

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
@@ -13,20 +13,14 @@ import  './firebase/firebase';
 
 const store = configureStore();
 
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }));
-store.dispatch(addExpense({ description: 'Power', amount: 2020, createdAt: -2000 }));
-store.dispatch(addExpense({ description: 'Water', amount: 200, createdAt: -25000 }));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
